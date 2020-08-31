0c8b04470faa9688a5a80fe7ab746d9e07de483b
Mac9330 <macyoung94@gmail.com>
2020-08-31 12:35:50 -0400

    fix build

---
+++ build/precache-manifest.da7f5d6a52a2b53541525517eaf03f8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9969c9b4d6e3a056d23ce5bb8852c5",
+    "url": "/mac9330/Kantan/index.html"
+  },
+  {
+    "revision": "f4e7de5f0d4688221b3c",
+    "url": "/mac9330/Kantan/static/css/main.8136a131.chunk.css"
+  },
+  {
+    "revision": "f99a63178fe0acca6bbd",
+    "url": "/mac9330/Kantan/static/js/2.7035feeb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mac9330/Kantan/static/js/2.7035feeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4e7de5f0d4688221b3c",
+    "url": "/mac9330/Kantan/static/js/main.d3485cf2.chunk.js"
+  },
+  {
+    "revision": "930186b9a558f5ddf62c",
+    "url": "/mac9330/Kantan/static/js/runtime-main.7153b9ba.js"
+  }
+]);
